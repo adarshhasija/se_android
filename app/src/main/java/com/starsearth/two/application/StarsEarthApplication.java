8ddc7d0c67b6655ec76abbc5061ccfd755df555c
adarshhasija <hasijaadarsh@gmail.com>
2020-07-06 21:14:02 +0530

    Added task: Office stationery

--- app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
+++ app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
@@ -102,7 +102,7 @@ public class StarsEarthApplication extends Application {
         //mDatabase.updateChildren(tagMap);
 
         //started at: 75
-        //highest id: 81
+        //highest id: 82
         //spelling type = 5
     }
 
