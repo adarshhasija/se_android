4edf53096ed09d008579d3d982cba883a3a7ff48
adarshhasija <hasijaadarsh@gmail.com>
2020-07-07 12:57:15 +0530

    New task: Words related to email

--- app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
+++ app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
@@ -102,7 +102,7 @@ public class StarsEarthApplication extends Application {
         //mDatabase.updateChildren(tagMap);
 
         //started at: 75
-        //highest id: 82
+        //highest id: 83
         //spelling type = 5
     }
 
