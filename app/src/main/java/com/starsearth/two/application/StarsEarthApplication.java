b4d76a8b3397d53b19b817a9ebf42bb542655d65
adarshhasija <hasijaadarsh@gmail.com>
2020-07-10 12:47:28 +0530

    New task 108: Words related to career

--- app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
+++ app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
@@ -102,7 +102,7 @@ public class StarsEarthApplication extends Application {
         //mDatabase.updateChildren(tagMap);
 
         //started at: 75
-        //highest id: 107
+        //highest id: 108
         //spelling type = 5
     }
 
