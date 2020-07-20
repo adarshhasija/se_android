fe7168ba3bf63439ee9a67d42d9280ab08b86e22
adarshhasija <hasijaadarsh@gmail.com>
2020-07-20 15:46:51 +0530

    New task 119: Money in the US New task 120: Measurements New task 121: Places and spaces in the house

--- app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
+++ app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
@@ -102,7 +102,7 @@ public class StarsEarthApplication extends Application {
         //mDatabase.updateChildren(tagMap);
 
         //started at: 75
-        //highest id: 118
+        //highest id: 121
         //spelling type = 5
     }
 
