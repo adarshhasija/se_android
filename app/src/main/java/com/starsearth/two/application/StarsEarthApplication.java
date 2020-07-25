392cf739eb59b1fbb87a25f8ab52dd3280f9717a
adarshhasija <hasijaadarsh@gmail.com>
2020-07-26 01:53:18 +0530

    New task 138: Cleaning agents. Spelling New task 139: Things in a bathing area. Spelling New task 140: The Living Room. Spelling New task 141: House pets and other animals. Spelling New task 142: The automobile. Spelling New task 143: Driving. Spelling New task 144: Types of Vehicles. Spelling New task 145: Breakfast. Spelling New task 146: Eatables in the west. Spelling New task 147: The Family. Spelling New task 148: Summer. Spelling New task 149: Fall/Autumn. Spelling New task 150: Winter. Spelling New task 151: Fish and Seafood. Spelling New task 152: Meat. Spelling New task 153: Mexican food. Spelling New task 154: Italian food. Spelling New task 155: Pasta. Spelling New task 156: Food related. Spelling New task 157: Baby and baby care. Spelling New task 158: Recreation Sports. Spelling

--- app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
+++ app/src/main/java/com/starsearth/two/application/StarsEarthApplication.java
@@ -102,7 +102,7 @@ public class StarsEarthApplication extends Application {
         //mDatabase.updateChildren(tagMap);
 
         //started at: 75
-        //highest id: 137
+        //highest id: 158
         //spelling type = 5
     }
 
